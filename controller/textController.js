a787a2a805376a615ef498545ab39f718ab42ebe
prewindemon <121555495@qq.com>
2016-02-17 14:50:01 +0800

    2

--- controller/textController.js
+++ controller/textController.js
@@ -1,9 +1,10 @@
 
+$.post("data/text.json",
+	function(data) {
+			alert(data);
+	},
+	'json');
 
-$.get('data/text.json').success(function(content){
-// content就为文件data.txt的文本内容了,注意txt文件的编码需要与html文件的编码一致，最好保存成utf-8
-	alert(content);
-});
 angular.module('phoneApp', [])
   .controller('PhoneListCtrl', function($scope) {
 	    $scope.phones = [
